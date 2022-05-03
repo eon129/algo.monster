ec18e493dfca049fd139b16ebd7139a3e077755e
Erick Olvera <erick.e.olvera@gmail.com>
2022-05-02 20:09:19 -0500

    updated excersice sqr root

--- 01_binary_search/square_root.cpp
+++ 01_binary_search/square_root.cpp
@@ -24,7 +24,9 @@ int square_root(int n) {
     while (l <= r)
     {
         int m = (l + r) / 2;
-        
+        //In boundary problems, the rule is to include the equals(=) comparation, this will give the true (T) elements.
+        //Result has to be in the boundary between F and T, but in the T element
+        //T cases can be in the left or right side of the list, it affects in the way l and r are 
         if ( n >= (m*m) ) {
             
             boundary = m;
