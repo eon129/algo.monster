f32d89cf13239fd3412abb608b8f493a280bc446
Erick Olvera <erick.e.olvera@gmail.com>
2022-08-01 19:57:01 -0500

    Added knapsack excersice first version

---
+++ 08_dynamic_prog/knapsack.cpp
@@ -0,0 +1,89 @@
+/*
+We want to discuss a classic dynamic programming problem, which is 0-1 knapsack. Given a series of objects with a weight and a value and a knapsack that can carry a set amount of weight, what is the maximum object value we can put in our knapsack without exceeding the weight constraint?
+
+Input
+weights: an array of integers that denote the weights of objects
+values: an array of integers that denote the values of objects
+max_weight: the maximum weight capacity of the knapsack
+Output
+the maximum value in the knapsack
+
+Examples
+Example 1:
+Input:
+
+weights = [3, 4, 7]
+values = [4, 5, 8]
+max_weight = 7
+Output: 9
+
+Explanation:
+
+We have a knapsack of max limit 7 with 3 objects of weight-value pairs of [3,4], [4,5], [7,8], then the maximal value we can achieve is using the first 2 objects to obtain value 4 + 5 = 9.
+
+The other possibilities would all be only 1 object in our knapsack, which would only yield values 4, 5, and 9.
+*/
+
+#include <algorithm> // copy
+#include <iostream> // cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int knapsack(std::vector<int> weights, std::vector<int> values, int max_weight) {
+    // WRITE YOUR BRILLIANT CODE HERE
+    
+    int max = 0;
+    
+    for (int i=0; i < weights.size(); i++) {
+    
+        if (weights[i] < max_weight) {
+            //Copy vectors
+            std::vector<int> w_temp = weights;
+            std::vector<int> v_temp = values;
+            w_temp.erase(w_temp.begin()+i);
+            v_temp.erase(v_temp.begin()+i);
+            //Iterate over all posibilities
+            int temp = knapsack(
+                w_temp,
+                v_temp,
+                max_weight - weights[i]) + values[i];
+            
+            //Store global max value
+            max = (temp>max) ? temp : max;
+            
+        } else if (weights[i] == max_weight) {
+            //If this condition is reached, only one element fits into the knapsack
+            max = (values[i]>max) ? values[i] : max;
+        } else {
+            //Do nothing
+        }
+    }
+    return max;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> weights = get_words<int>();
+    std::vector<int> values = get_words<int>();
+    int max_weight;
+    std::cin >> max_weight;
+    ignore_line();
+    int res = knapsack(weights, values, max_weight);
+    std::cout << res << '\n';
+}
