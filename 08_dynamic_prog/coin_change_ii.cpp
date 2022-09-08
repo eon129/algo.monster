24e2456a509e251a90c3b09f3655cf71be50a9b1
Erick Olvera <erick.e.olvera@gmail.com>
2022-09-08 13:07:32 -0500

    Updated coin chagne ii

--- 08_dynamic_prog/coin_change_ii.cpp
+++ 08_dynamic_prog/coin_change_ii.cpp
@@ -76,13 +76,41 @@ int top_down(std::vector<int> &coins, int amount, std::vector<std::vector<int>> 
     return res;
 }
 
+int bottom_up(std::vector<int> &coins, int amount) {
+
+    std::vector<std::vector<int>> dp(coins.size()+1, std::vector<int>(amount+1, 0));
+    
+    //Base case, there is only one way to make amount 0 with 0 coins
+    dp[0][0] = 1;
+    
+    //Start with i=1, makes no sense to evaluate when we have 0 coins
+    for (int i=1; i<=coins.size(); i++) {
+    
+        for (int j=0; j<=amount; j++) {
+            
+            //First evaluate current position with combinations in prev i-1 position
+            dp[i][j] = dp[i-1][j];
+            
+            if ( j - coins[i-1] >=0) {
+                
+                //Sum new result with past combinations
+                dp[i][j] += dp[i][j - coins[i-1]];
+            }
+        }
+    }
+    
+    return dp[coins.size()][amount];
+}
+
 int coin_game(std::vector<int> coins, int amount) {
     // WRITE YOUR BRILLIANT CODE HERE
     
     int res;
     
-    std::vector<std::vector<int>> memo(coins.size()+1, std::vector<int>(amount+1, -1));
-    res = top_down(coins, amount, memo, 0);
+    //std::vector<std::vector<int>> memo(coins.size()+1, std::vector<int>(amount+1, -1));
+    //res = top_down(coins, amount, memo, 0);
+    
+    res = bottom_up(coins, amount);
     
     return res;
 }
